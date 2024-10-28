a458f7ea9305ca7d862b13f1d8a01854244fedb6
victor <victorgomesdacosta18@gmail.com>
2024-10-28 09:44:42 -0300

    feat:to finish the form sign-up

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -3,16 +3,18 @@ import React from 'react'
 const RegisterForm = () => {
   return (
     <div className="h-screen w-full flex items-center justify-center">
-        <div className="flex  flex-col h-full w-96 ">
-        <h1 className="text-xl font-bold">Cadastre a tarefa:</h1>
-        <form className="flex flex-col">
+        <div className="flex  flex-col h-full w-96 mt-10">
+        <h1 className="text-xl font-bold mb-4">Cadastre a tarefa:</h1>
+        <form className="flex flex-col h-full gap-y-4">
             <label>Digite o nome da tarefa:</label>
             <input type='text' />
             <label>Digite o nome do autor:</label>
             <input />
             <label>Digite o estado da tarefa:</label>
             <input />
-            <button>Cadastrar</button>
+            <div className="flex items-center justify-center">
+              <button className="bg-green-600 h-8 rounded-xl max-w-24 p-1">Cadastrar</button>
+            </div>
         </form>
     </div>
     </div>
