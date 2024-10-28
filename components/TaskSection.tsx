80c32e8b2e49cfb2bc6bf9d98a91da05482ec9c9
victor <victorgomesdacosta18@gmail.com>
2024-10-27 23:00:28 -0300

    feat: title

--- components/TaskSection.tsx
+++ components/TaskSection.tsx
@@ -14,7 +14,7 @@ const TaskSection = () => {
             <div className="flex items-center gap-6 h-12 font-bold text-zinc-200 bg-zinc-800 border border-green-500 rounded-lg p-4 mt-3">
                 <p className='w-full ml-3'>Name</p>
                 <p className="w-full">Carlos</p>
-                <p className="w-full">building</p>
+                <p className="w-full">stop</p>
                 <p className='w-full'>26/10/2024</p>
             </div>
             <div className="flex items-center gap-6 h-12 font-bold text-zinc-200 bg-zinc-800 border border-green-500 rounded-lg p-4 mt-3">
