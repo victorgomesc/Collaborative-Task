f6fe881641a671e15c0d99790d8338455bea8de1
victor <victorgomesdacosta18@gmail.com>
2024-10-29 08:39:30 -0300

    feat: implement mutation in register form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,14 +1,22 @@
+"use client"
+
 import Header from '@/components/Header'
 import HeaderTitle from '@/components/HeaderTitle'
 import RegisterForm from '@/components/RegisterForm'
 import React from 'react'
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from '@/lib/react-query';
+
 
 export default function Register() {
   return (
     <div className='text-white'>
-      <HeaderTitle />
-      <Header />
-      <RegisterForm />
+      <QueryClientProvider client={queryClient}>
+        <HeaderTitle />
+        <Header />
+        <RegisterForm />
+      </QueryClientProvider>
+      
     </div>
   )
 }
